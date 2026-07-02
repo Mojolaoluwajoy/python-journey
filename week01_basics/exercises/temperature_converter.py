5ce88fecbb04807cab15ac7ae79a41efd931d6d3
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-02 20:11:48 +0100

    feat: add temperature converter

---
+++ week01_basics/exercises/temperature_converter.py
@@ -0,0 +1,8 @@
+Celcius = int(input("Enter temperature in celcius: "))
+
+Fahrenheit =(Celcius * 9/5) + 32
+
+kelvin = Celcius +273.15
+
+print(f"Fahrenheit : {Fahrenheit: .2f}")
+print(f"Kelvin : {kelvin: .2f}")
