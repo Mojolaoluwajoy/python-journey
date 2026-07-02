7f7e1062829b366573426694c622b4af63dbf5c5
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-02 20:02:01 +0100

    feat:complete seconds converter

--- week01_basics/exercises/seconds_converter.py
+++ week01_basics/exercises/seconds_converter.py
@@ -1,5 +1,6 @@
 seconds = int(input("Enter seconds: "))
 hour=seconds//3600
-remaining_seconds=seconds/3600
-
-print("")
+remaining=seconds % 3600
+minute =remaining // 60
+remaining_seconds = remaining % 60
+print(f"{seconds} seconds = {hour} hour(s), {minute} minute(s), and {remaining_seconds} second(s)")
