00317f8d28a7985c1fdbc3626a1204c3502d95e2
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-06 17:11:54 +0100

    feat:add word and character counter class

--- week02_strings/exercises/word_and_character_counter.py
+++ week02_strings/exercises/word_and_character_counter.py
@@ -1,1 +1,21 @@
-c
+sentence = input ("Enter a sentence: ")
+
+print("Uppercase: ",sentence.upper())
+
+word = sentence.split()
+word_count =len(word)
+print("Word count: ",word_count)
+
+no_space= sentence.replace(" ","")
+char_count= len(no_space)
+print("Char count: ",char_count)
+
+vowels ="aeiou"
+count =sum(1 for char in sentence.lower() if char in vowels)
+print("Vowel count: ",count)
+words =sentence.split()
+
+lengths =[len(words)]
+longest_word =max(words,key =len)
+print("Longest word: ",longest_word)
+
