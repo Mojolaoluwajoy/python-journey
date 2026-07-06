c1a7666f46ac381856ae55acf0704ad393b117dd
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-06 23:20:00 +0100

    fix : update word and character counter class

--- week02_strings/exercises/word_and_character_counter.py
+++ week02_strings/exercises/word_and_character_counter.py
@@ -13,8 +13,8 @@ print("Char count: ",char_count)
 vowels ="aeiou"
 count =sum(1 for char in sentence.lower() if char in vowels)
 print("Vowel count: ",count)
-words =sentence.split()
 
+words =sentence.split()
 lengths =[len(words)]
 longest_word =max(words,key =len)
 print("Longest word: ",longest_word)
