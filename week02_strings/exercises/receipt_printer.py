3cd266e0c0fedb953b178f9d8d7f8793c7fd37fc
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-06 18:58:41 +0100

    feat:add receipt printer

---
+++ week02_strings/exercises/receipt_printer.py
@@ -0,0 +1,6 @@
+product_name = input("Enter the product name: ")
+product_price = int(input("Enter the product price: "))
+
+print("===================================================")
+print(f"{product_name:<25} {product_price:>12.2f}")
+print("===================================================")
