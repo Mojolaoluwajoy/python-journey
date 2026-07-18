f33a13e504ba0241ba30a687bd266892f5de4239
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-18 09:28:07 +0100

    feat: add list slicing

---
+++ week03_lists_tuples/examples/list_slicing.py
@@ -0,0 +1,15 @@
+scores = [92,78,85,90,67,86,95]
+
+print(scores[0:3])
+
+print(scores[2:5])
+
+print(scores[:3])
+
+print(scores[3:])
+
+print(scores[-3:])
+
+print(scores[::2])
+
+print(scores[::-1])
