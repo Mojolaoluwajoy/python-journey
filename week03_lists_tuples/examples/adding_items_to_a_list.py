e9fa20ac80dc67e85b5e51cb7b65a03e9f45ce14
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-18 09:42:26 +0100

    feat: add adding items to a list

---
+++ week03_lists_tuples/examples/adding_items_to_a_list.py
@@ -0,0 +1,11 @@
+languages = ['Python','Java']
+print(languages)
+
+languages.append('C++')
+print(languages)
+
+languages.insert(1,'Kotlin')
+print(languages)
+
+languages.extend(['Ruby','Go'])
+print(languages)
