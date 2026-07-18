f535fd775e1d85e21b6d0ba687334dc9d2f7e813
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-18 12:30:55 +0100

    feat: add sorting list

---
+++ week03_lists_tuples/examples/sorting_lists.py
@@ -0,0 +1,11 @@
+languages = ['Python','Java','C++','JavaScript','Ruby']
+
+sorted_languages = sorted(languages)
+print(sorted_languages)
+print(languages)
+
+languages.sort()
+print(languages)
+
+languages.sort(reverse=True)
+print(languages)
