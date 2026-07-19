a6fa3314b95f566fac441115a08fee75592d9aa4
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-19 21:24:44 +0100

    feat: add builtin list functions class

---
+++ week03_lists_tuples/examples/built_in_list_funtions.py
@@ -0,0 +1,14 @@
+scores = [92, 78, 85, 90, 67, 88, 95]
+
+print(len(scores))
+
+print(min(scores))
+
+print(max(scores))
+
+print(max(scores))
+
+print(sum(scores))
+
+average = sum(scores)/len(scores)
+print(f'Average: {average:.2f}')
