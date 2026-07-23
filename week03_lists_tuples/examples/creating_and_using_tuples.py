db70a98580d15ee0fad365db585d3d9f4ce6ddd9
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-23 17:25:00 +0100

    feat:add creating_and_using_tuples to show how tuples are created and used

---
+++ week03_lists_tuples/examples/creating_and_using_tuples.py
@@ -0,0 +1,15 @@
+coordinates = (10.5,6.3)
+
+rgb_red = (255,0,0)
+
+days =('Mon','Tue','Wed','Thur','Fri','Sat','Sun')
+
+print(coordinates[0])
+
+print(days[-1])
+
+print(days[1:4])
+
+x, y = coordinates
+
+print(f'x={x}, y={y}')
