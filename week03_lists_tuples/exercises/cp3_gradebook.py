40a7434e9b7480b660dc8eef5c7d47334589c13d
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-07-30 09:15:15 +0100

    feat:create a student list consisting of each student's name and scores

--- week03_lists_tuples/exercises/cp3_gradebook.py
+++ week03_lists_tuples/exercises/cp3_gradebook.py
@@ -1,3 +1,25 @@
-student_names = ['Mojlaoluwa Joy','Oluwapelumi Babatunde','Omobolanle Victoria']
+students=[
 
-student_scores =[100,95,90]
+    ['Mojolaoluwa Joy',[92,88,5,79,85]],
+    ["Oluwapelumi Babatunde",[78,82,75,80,77]],
+    ['Omobolanle Victoria',[65,70,68,72,60]]
+
+    ]
+
+
+
+
+for student in students:
+    name = student[0]
+    score = student[1]
+    summation = sum(score)
+    average = summation/5
+    high_score = max(score)
+    low_score = min(score)
+
+    print(f"{name}: {score}")
+    print(f'summation: {summation}')
+    print(f'average: {average}')
+    print(f'highest score: {high_score}')
+    print(f'lowest score: {low_score}')
+
