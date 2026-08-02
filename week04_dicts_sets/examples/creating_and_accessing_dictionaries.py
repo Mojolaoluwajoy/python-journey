36d2eb6ae3e724623fb006f0cbb7ad48e7985064
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-08-02 23:56:25 +0100

    feat:add creating_and_accessing_dictionaries

---
+++ week04_dicts_sets/examples/creating_and_accessing_dictionaries.py
@@ -0,0 +1,15 @@
+
+
+student = {
+
+    'name' : 'Mojolaoluwa',
+    'age' : 27,
+    'university': 'Miva Opn University',
+    'gpa': 3.8
+}
+
+print(student['name'])
+print(student['age'])
+print(student['university'])
+
+print(f"{student['name']}has a  GPA of {student['gpa']}")
